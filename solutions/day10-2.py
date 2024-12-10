093d1cfe53d72b640d5aa24bafb4d0cb37e453f8
Jude Young <jude.d.l.young@icloud.com>
2024-12-10 15:30:48 +0000

    day 10

---
+++ solutions/day10-2.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+def find_zero(grid, position):
+    height = grid[position[0], position[1]]
+    if height == 0:
+        yield position
+    safe_grid = np.pad(grid, 1, mode='constant', constant_values=10)
+    if safe_grid[1+position[0], 1+position[1]+1] == height - 1:
+        yield from find_zero(grid, [position[0], position[1]+1])
+    if safe_grid[1+position[0], 1+position[1]-1] == height -1:
+        yield from find_zero(grid, [position[0], position[1]-1])
+    if safe_grid[1+position[0]+1, 1+position[1]] == height - 1:
+        yield from find_zero(grid, [position[0]+1, position[1]])
+    if safe_grid[1+position[0]-1, 1+position[1]] == height -1:
+        yield from find_zero(grid, [position[0]-1, position[1]])
+
+INPUT = './input/day10-1.txt'
+grid = np.array([[int(element) for element in row[:-1] ]for row in open(INPUT).readlines()])
+output = np.zeros(shape=np.shape(grid))
+# print(grid)
+for position in [[x, y] for x in range(np.shape(grid)[0]) for y in range(np.shape(grid)[1])]:
+    if grid[position[0], position[1]] == 9:
+        for result in find_zero(grid, position):
+            output[result[0], result[1]] += 1 
+# print(output)
+print(np.sum(output))
+
+from utils.progress import Printer
+p = Printer()
+p.arrprint(grid)
+p.arrprint(output, True, bgmode='g')
